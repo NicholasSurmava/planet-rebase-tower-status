5b615f14654cd0c0311791ddce4d13f6e98c7cf9
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-09-23 19:50:24 -0500

    added basic form submission to return a site page, created base template and site tempalte

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,4 +6,13 @@ app = Flask(__name__)
 def index():
     title = "Tower Status Checker"
 
+    if request.method == "POST":
+        site = request.form['site']
+        print(site)
+        return redirect(url_for("site_status", site=site))
+
     return render_template('index.html', title=title)
+
+@app.route('/<site>')
+def site_status(site):
+    return render_template('site.html', site=site)
