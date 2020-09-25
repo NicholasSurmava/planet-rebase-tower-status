e9d8f28f65265eb9676fadbff0ba49e022fd073a
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-09-24 19:41:31 -0500

    added mock data and rendered on site page

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os
+import os, json
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', "super-secret-dev-key")
 
+def search_site(site_id):
+    site_id = int(site_id)
 
+    with open('MOCK_DATA.json') as json_file:
+        data = json.load(json_file)
+        for s in data:
+            for k, v in s.items():
+                if v == site_id:
+                    return s
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,6 +31,7 @@ def index():
 def site_status(site):
     if "user" in session:
         user = session["user"]
+        site = search_site(site)
         return render_template('site.html', site=site, user=user)
     
     return redirect(url_for("index"))
