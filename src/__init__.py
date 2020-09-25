8586c7db6ade5464a67aff7c83075d67f9c0ce6c
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-09-25 17:23:56 -0500

    added a fake weather function to test 'slow queries'

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os, json
+import os, json, time
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', "super-secret-dev-key")
 
+warehouse_data = 'data_warehouse.json'
+weather_data = ' '
+
 def search_site(site_id='all'):
     if site_id == 'all':
-        with open('MOCK_DATA.json') as json_file:
+        with open(warehouse_data) as json_file:
             data = json.load(json_file)
             return data
     else:
         site_id = int(site_id)
 
-        with open('MOCK_DATA.json') as json_file:
+        with open(warehouse_data) as json_file:
             data = json.load(json_file)
             for s in data:
                 for k, v in s.items():
                     if v == site_id:
                         return s
+
+def get_weather(site_id='all'):
+    time.sleep(3)
+    return 'hot'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -37,10 +44,12 @@ def index():
 def site_status(site):
     if "user" in session:
         user = session["user"]
+
         site = search_site(site)
+        weather = get_weather()
 
         if site != None:
-            return render_template('site.html', site=site, user=user)
+            return render_template('site.html', user=user, site=site, weather=weather)
         else:
             return '<p>Unknown Site</p>'
     
